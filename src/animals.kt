591da50e65b10437f3775ef36e042da7cdaa8fcd
Shoonsk8 <shoon@att.net>
2019-06-04 17:50:08 -0400

    finsihed all

--- src/animals.kt
+++ src/animals.kt
@@ -1,45 +1,63 @@
 sealed class Animals(val id:Int, val name:String, var yearDiscovered:Int){
 
 
+
     fun consumeFood( food:String){
 
     }
 
-    open fun move(){
+    open fun move():String{
+        return "move"
+    }
+
+    open fun breath():String{
+        return "breathe"
 
     }
 
-    fun breath(){
-
-    }
-
-    fun reprduce(){
-
+    open fun reprduce():String{
+        return "reproduce"
     }
 }
 
 class Mammals(id: Int, name: String, yearDiscovered: Int): Animals(id, name, yearDiscovered) {
-    override fun move(){
-
+    override fun move():String{
+        return "usually walk"
     }
 
+    override fun breath():String{
+        return "breathe with lungs"
 
-
+    }
+    override fun reprduce():String{
+        return "live births except for platypus"
+    }
 }
 
 
 class Birds(id: Int, name: String, yearDiscovered: Int): Animals(id, name, yearDiscovered) {
-    override fun move(){
+    override fun move():String{
+        return "usually fly"
+    }
+    override fun breath():String{
+        return "breathe with lungs"
 
     }
-
-
+    override fun reprduce():String{
+        return "egg"
+    }
 
 }
 
 class Fish(id: Int, name: String, yearDiscovered: Int): Animals(id, name, yearDiscovered) {
-    override fun move() {
+    override fun move() :String{
+        return "usually swim"
+    }
+    override fun breath():String{
+        return "breathe with branchia"
 
     }
-
+    override fun reprduce():String{
+        return "egg except for sea horse,guppy etc."
+    }
 }
--- src/main.kt
+++ src/main.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
 
     animals.add(Mammals(1,"Panda",1869))
 
-    animals.add(Mammals(2,"Zebra", ,1778))
+    animals.add(Mammals(2,"Zebra", 1778))
     animals.add(Mammals(3,"Koala", 1816))
     animals.add(Mammals(4,"Sloth", 1804))
     animals.add(Mammals(5,"Armadillo ", 1758))
@@ -19,5 +19,50 @@ fun main(args: Array<String>) {
     animals.add(Fish(13,"Salmon ", 1758))
     animals.add(Fish(14,"Catfish ", 1817))
     animals.add(Fish(15,"Perch ", 1758))
+    for(i in 0..animals.size-1){
+        println("name is ${animals[i].name}")
+    }
+
+    animals.sortBy {it.yearDiscovered }
+
+    for(i in 0..animals.size-1){
+        println("name is ${animals[i].name} discovered in ${animals[i].yearDiscovered} ")
+    }
+    animals.sortedBy { it.name }
+
+    for(i in 0..animals.size-1){
+        println("name is ${animals[i].name}")
+    }
+
+    animals.sortBy {it.move() }
+
+    for(i in 0..animals.size-1){
+        println("name is ${animals[i].name} , which ${animals[i].move()}")
+    }
+
+    var filteredanimal=animals.filter { it->it.breath().contains("lungs") }
+
+    for(i in 0..filteredanimal.size-1){
+        println("name is ${filteredanimal[i].name} , which ${filteredanimal[i].move()} and ${filteredanimal[i].breath()} " )
+    }
+
+    filteredanimal=filteredanimal.filter { it->it.yearDiscovered==1758 }
+
+    for(i in 0..filteredanimal.size-1){
+        println("name is ${filteredanimal[i].name} , which ${filteredanimal[i].move()} and ${filteredanimal[i].breath()} discovered in ${filteredanimal[i].yearDiscovered} " )
+    }
+    filteredanimal=filteredanimal.filter { it->it.reprduce().contains("egg")&&it.breath().contains("lungs") }
+
+    for(i in 0..filteredanimal.size-1){
+        println("name is ${filteredanimal[i].name} , which ${filteredanimal[i].move()} and ${filteredanimal[i].breath()} reproduce with ${filteredanimal[i].reprduce()} " )
+    }
+
+    filteredanimal=filteredanimal.filter { it->it.yearDiscovered==1758 }
+    filteredanimal.sortedBy { it.name }
+
+    for(i in 0..filteredanimal.size-1){
+        println("name is ${filteredanimal[i].name} , which ${filteredanimal[i].move()} and ${filteredanimal[i].breath()} discovered in ${filteredanimal[i].yearDiscovered} " )
+    }
+
 
 }
